aab01f3b9706d5767124bec9c8ec46b0af7f770e
Kakeru <stand.by.we@gmail.com>
2021-10-04 16:40:53 +0900

    ガワ

---
+++ KeychainApp/KeychainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainAppApp.swift
+//  KeychainApp
+//
+//  Created by Kakeru Fukuda on 2021/10/04.
+//
+
+import SwiftUI
+
+@main
+struct KeychainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
